ed089a73dbf94112fd503a8e626bb7ae94fa8fe6
My Name <thesame@email.com>
2022-05-17 11:23:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from 'react'
 
 function App() {
+
+  const [name , setName] = useState('')
+
+  const prevName = useRef('')
+ // const [renderCount, setRenderCount] = useState(0)
+
+  useEffect (()=>{
+
+    // ComponentDidMount () & ComponentDidUpdate ()
+
+    prevName.current = name
+
+
+    return () =>{
+
+      // code here for ComponentWillUnmount
+
+    }
+
+  },[name])
+
+  // ComponentDidMount () : Dependency Array should be written as an empty Array
+
+  // ComponentDidUpdate() : without dependency Array => useEffect() method after every render() methods runs.
+
+  // ComponentDidUpdate() : with Dependency Array => method depending on the changing state
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    <input value={name} onChange={e=> setName(e.target.value)}/>
+
+    <div> HALLLOOO HOW ARE YOU.... {name} and it used to be {prevName.current} </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
